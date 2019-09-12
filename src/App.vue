d9940108e89c5a7c04f742e9843613695d29ba2a
Satoshi Nagasu <s-nagasu.03@nexsimo.co.jp>
2019-09-12 23:03:39 +0900

    update

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,10 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/checkbox">CheckBox</router-link>
     </div>
+    <hr>
     <router-view />
   </div>
 </template>
---
+++ src/components/atoms/AtomCheckBox.vue
@@ -0,0 +1,53 @@
+<template>
+  <label :class="classes">
+    <input
+      v-model="internalValue"
+      type="checkbox"
+      :name="name"
+      :value="value"
+    />{{ label }}
+  </label>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component({
+  model: {
+    prop: "checked",
+    event: "change"
+  }
+})
+export default class AtomCheckBox extends Vue {
+  @Prop({ type: String })
+  name!: string;
+
+  @Prop({ type: String })
+  label!: string;
+
+  @Prop({ type: [Boolean, Array] })
+  checked!: boolean | string[];
+
+  @Prop({ type: String })
+  value!: string;
+
+  @Prop({ type: Boolean, default: false })
+  small!: boolean;
+
+  get internalValue(): boolean | string[] {
+    return this.checked;
+  }
+
+  set internalValue(newVal: boolean | string[]) {
+    this.$emit("change", newVal);
+  }
+
+  get classes() {
+    return {
+      small: this.small
+    };
+  }
+}
+</script>
+
+<style scoped lang="scss"></style>
