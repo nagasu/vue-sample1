1223c37b60e153fc8b5c57e43bdf339ec8d8e6c1
Satoshi Nagasu <s-nagasu.03@nexsimo.co.jp>
2020-01-27 21:06:03 +0900

    update

--- src/router.ts
+++ src/router.ts
@@ -1,32 +1,38 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Home from "./views/Home.vue";
-import CheckBox from "@/views/CheckBox.vue";
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+import CheckBox from '@/views/CheckBox.vue'
+import Flex from '@/views/Flex.vue'
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
-      path: "/",
-      name: "home",
+      path: '/',
+      name: 'home',
       component: Home
     },
     {
-      path: "/about",
-      name: "about",
+      path: '/about',
+      name: 'about',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+        import(/* webpackChunkName: "about" */ './views/About.vue')
     },
     {
-      path: "/checkbox",
-      name: "checkbox",
+      path: '/checkbox',
+      name: 'checkbox',
       component: CheckBox
+    },
+    {
+      path: '/flex',
+      name: 'flex',
+      component: Flex
     }
   ]
-});
+})
